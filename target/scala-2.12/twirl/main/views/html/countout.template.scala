b902687b3be0ecc20c00fd5d7d06bd94e2111d4a
Levidu <levidualahakoon@gmail.com>
2019-08-22 03:11:43 +0530

    fixed the UI [count]

--- target/scala-2.12/twirl/main/views/html/countout.template.scala
+++ target/scala-2.12/twirl/main/views/html/countout.template.scala
@@ -87,7 +87,7 @@ Seq[Any](format.raw/*2.1*/("""<html>
                 <form action=""""),_display_(/*56.32*/routes/*56.38*/.HomeController.count_out_save()),format.raw/*56.70*/("""" method="POST">
                     <div class="btn-group btn-group-justified" data-toggle="buttons">
 					<label class="btn btn-primary btn-lg"> <input type="radio"
-						name="user" value="user1" required>User 1
+						name="user" value="user1" oninvalid="alert('අසම්පූර්ණයි! දත්ත නැවත ඇතුල් කරන්න');" required>User 1
 					</label> <label class="btn btn-primary btn-lg"> <input type="radio"
 						name="user" value="user2">User 2
 					</label> <label class="btn btn-primary btn-lg"> <input type="radio"
@@ -102,13 +102,13 @@ Seq[Any](format.raw/*2.1*/("""<html>
 				</div>
                     <br>
                     <br>
-                    <input type="text" class="form-control" placeholder="EGG COUNT | හොඳ බිත්තර" name="egg_count" id="egg_count" required>
+                    <input type="text" class="form-control" placeholder="EGG COUNT | බිත්තර ගණන" name="egg_count" id="egg_count" oninvalid="alert('දත්ත ඇතුළත් කර නැත');" required>
                     <br>
-                    <input type="text" class="form-control" placeholder="DAMAGE COUNT | හානි බිත්තර" name="damage_count" id="damage_count" required>
+                    <input type="text" class="form-control" placeholder="DAMAGE COUNT | කැඩුණු බිත්තර" name="damage_count" id="damage_count" oninvalid="alert('දත්ත ඇතුළත් කර නැත');" required >
                     <br>
-                    <input type="text" class="form-control" placeholder="DIRTY COUNT | අපිරිසිදු බිත්තර" name="dirty_count" id="dirty_count" required>
+                    <input type="text" class="form-control" placeholder="DIRTY COUNT | අපිරිසිදු බිත්තර" name="dirty_count" id="dirty_count" oninvalid="alert('දත්ත ඇතුළත් කර නැත');" required >
                     <br>
-                    <input type="text" class="form-control" placeholder="DAMAGED BY USER | මගේ හානි බිත්තර" name="damaged_on_counter" id="damaged_on_counter" required>
+                    <input type="text" class="form-control" placeholder="DAMAGED BY USER | පරිශීලකයාගේ බිත්තර හානිය" name="damaged_on_counter" id="damaged_on_counter" oninvalid="alert('දත්ත ඇතුළත් කර නැත');" required>
                     <br>
                     <br>
                     <br>
@@ -140,9 +140,9 @@ Seq[Any](format.raw/*2.1*/("""<html>
 
               /*
                   -- GENERATED --
-                  DATE: Wed Aug 21 06:37:06 IST 2019
+                  DATE: Thu Aug 22 03:07:10 IST 2019
                   SOURCE: /Users/levi/Downloads/medilandaFarmApp/app/views/countout.scala.html
-                  HASH: 0c5daf6eb7a3b8164d0e2d37763fecf4a7390e14
+                  HASH: dcb8bcc9fb6cb88f70332f1f2ac727ac3c6a3ac1
                   MATRIX: 962->1|1095->41|1285->205|1299->211|1363->255|1430->296|1444->302|1525->362|1691->501|1706->507|1795->574|1847->599|1862->605|1925->647|1986->681|2001->687|2082->747|2143->781|2158->787|2245->853|2306->887|2321->893|2401->952|2462->986|2477->992|2567->1061|2647->1113|2676->1114|2717->1127|2769->1151|2798->1152|2843->1169|2899->1197|2928->1198|2957->1199|2995->1210|3024->1211|3053->1212|3107->1238|3136->1239|3177->1252|3232->1279|3261->1280|3306->1297|3362->1325|3391->1326|3420->1327|3458->1338|3487->1339|3516->1340|3570->1366|3599->1367|3640->1380|3694->1406|3723->1407|3768->1424|3824->1452|3853->1453|3882->1454|3920->1465|3949->1466|3978->1467|4032->1493|4061->1494|4102->1507|4163->1540|4192->1541|4237->1558|4293->1586|4322->1587|4351->1588|4389->1599|4418->1600|4447->1601|5001->2128|5016->2134|5069->2166
                   LINES: 28->1|33->2|37->6|37->6|37->6|38->7|38->7|38->7|41->10|41->10|41->10|42->11|42->11|42->11|43->12|43->12|43->12|44->13|44->13|44->13|45->14|45->14|45->14|46->15|46->15|46->15|48->17|48->17|49->18|49->18|49->18|50->19|51->20|51->20|51->20|52->21|52->21|52->21|53->22|53->22|54->23|54->23|54->23|55->24|56->25|56->25|56->25|57->26|57->26|57->26|58->27|58->27|59->28|59->28|59->28|60->29|61->30|61->30|61->30|62->31|62->31|62->31|63->32|63->32|64->33|64->33|64->33|65->34|66->35|66->35|66->35|67->36|67->36|67->36|87->56|87->56|87->56
                   -- GENERATED --
